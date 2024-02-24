20fce883e6c0a949211551e8a8819bd5af7308a1
Felix Zhu <felix.zhushu@gmail.com>
2024-02-24 17:32:34 +0800

    Update square.py

--- square.py
+++ square.py
@@ -1,16 +1,19 @@
 class Shape:
-def __init__(self, name, length):
-return def area():
-return 0
+        def __init__(self, name, length):
+                return
+        def area():
+                return 0
+                
 class Square(Shape):
-def __init__(self, name, length):
-        self.name = name
-        self.length = length
-def area(self):
-print('The area is:\n')
-return self.length * self.length
-def describe(self):
-return '\nThis is a:' + self.name
+        def __init__(self, name, length):
+                self.name = name
+                self.length = length
+        def area(self):
+                print('The area is:\n')
+                return self.length * self.length
+        def describe(self):
+                return '\nThis is a:' + self.name
+                
 s = Square('square',5)
 print(s.area())
 print(s.describe())
